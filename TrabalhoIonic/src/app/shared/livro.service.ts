b355310ca79c269b310405d4f4d97e0938f70378
Joao_Paulo_Preis <joaopaulopreis@gmail.com>
2020-12-10 20:25:38 -0300

    AtividadeFinal

--- TrabalhoIonic/src/app/shared/livro.service.ts
+++ TrabalhoIonic/src/app/shared/livro.service.ts
@@ -25,7 +25,7 @@ export class LivroService {
                 titulo: 'A arte de escrever',
                 foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/81bnAIdhStL.jpg',
                 autor:  'Arthur Schopenhaur',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SDDE4DES5SR'
                 
             },
             {
@@ -33,63 +33,63 @@ export class LivroService {
                 titulo: 'As virtudes Morais',
                 foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/515XukOBCnL.jpg',
                 autor:  'Santo Tomás de Aquino',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SDS5ERD652V'
                 
             },
             {
                 id_livro: 4,
                 titulo: 'Guerra e Paz',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://m.media-amazon.com/images/I/41z1EV7E2aL.jpg',
                 autor:  'Liev Tolstói',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SDW4SDWERF5'
                 
             },
             {
                 id_livro: 5,
                 titulo: 'Democracia, o Deus que Falhou',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/91l5clkTUwL.jpg',
                 autor:  'Hans-Hermann Hoppe',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'DERF5D48FRD'
                 
             },
             {
                 id_livro: 6,
                 titulo: 'A anatomia do estado',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://www.letracapital.com.br/wp-content/uploads/2014/12/238-A-anatomia-do-estado.jpg',
                 autor:  'Murray Rothbard',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SSDFER547FD'
                 
             },
             {
                 id_livro: 7,
                 titulo: 'O Capital',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://m.media-amazon.com/images/I/51rekLx0xtL.jpg',
                 autor:  'Karl Marx',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SFWER1585SD'
                 
             },
             {
                 id_livro: 8,
                 titulo: 'Atlas Shrugged',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/612URtxh-qL.jpg',
                 autor:  'Ayn Rand',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SF5874DFE5S'
                 
             },
             {
                 id_livro: 9,
                 titulo: 'O Senhor dos Anéis',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/51CDlkHVkaL._SX354_BO1,204,203,200_.jpg',
                 autor:  'J. R. R. Tolkien',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SFSFWR58721'
                 
             },
             {
                 id_livro: 10,
                 titulo: 'A Psicanálise dos Contos de Fadas',
-                foto_livro: 'https://cdn.ome.lt/zn4uV-QEUyGTaVIe2yKzijUXFEo=/670x0/smart/uploads/conteudo/fotos/dungeons-and-dragons-livro-do-jogador.jpg',
+                foto_livro: 'https://images-na.ssl-images-amazon.com/images/I/81R1KU2OBSL.jpg',
                 autor:  'Bruno Bettelheim',
-                isbn: 'SDW4S5WE52S'
+                isbn: 'SSFAFWER96S'
                 
             }
             
